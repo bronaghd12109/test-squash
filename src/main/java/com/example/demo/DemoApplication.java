983bb56814ced52040a04584cc59b0befc3e0fbe
Bronagh Doherty <Doherty-B28@ulster.ac.uk>
2021-10-25 11:02:39 +0000

    Feature-123

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +17,11 @@ public class DemoApplication {
     @GetMapping("/hello-spring")
     String greet(){
         return "Hello, Spring!";
+    }
+
+    @PostMapping("/test")
+    String test(){
+        return "test";
     }
 
 }
